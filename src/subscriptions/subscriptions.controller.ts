719319fcef05ae65951dcb9bbb1061d92e171e4e
G M Ganesh <ganesh.gm@magnificenztech.com>
2025-09-05 13:28:59 +0530

    Log check

--- src/subscriptions/subscriptions.controller.ts
+++ src/subscriptions/subscriptions.controller.ts
@@ -8,11 +8,12 @@ import {
   Delete,
   Headers,
   Res,
+  Req,
 } from '@nestjs/common';
 import { SubscriptionsService } from './subscriptions.service';
 import { CreateSubscriptionDto } from './dto/subscription.dto';
 import { Response } from 'express';
-import * as crypto from 'crypto'
+import * as crypto from 'crypto';
 
 @Controller('subscription')
 export class SubscriptionsController {
@@ -29,34 +30,37 @@ export class SubscriptionsController {
 
   @Post('webhook')
   async handleCashfreeWebhook(
-    @Body() body: any,
+    @Req() req: Request,
     @Headers('x-webhook-signature') signature: string,
     @Res() res: Response,
   ) {
     try {
-      const payload = JSON.stringify(body);
-      console.log(payload)
+      // ✅ Get raw body as string
+      const rawBody =
+        (req as any).rawBody?.toString() || JSON.stringify(req.body);
+
       const expectedSignature = crypto
         .createHmac('sha256', process.env.CASH_FREE_SECRET_KEY!)
-        .update(payload)
+        .update(rawBody)
         .digest('base64');
-      console.log(signature,"Signature")
-      console.log(expectedSignature,"expectedSignature");
+
+      console.log(signature, 'Signature');
+      console.log(expectedSignature, 'expectedSignature');
+
       if (signature !== expectedSignature) {
-        
         return res.status(400).send('Invalid signature');
       }
 
+      const body :any= req.body;
+
       // ✅ Process event
-      console.log('body.data?.order_status', body.data?.order_status)
-      if (body.data?.order_status === 'PAID') {
+      if (body.type === 'PAYMENT_SUCCESS_WEBHOOK') {
         await this.subscriptionsService.activateSubscription(
-          body.data.order_id,
-          body.data.payment_id,
-          body.data.order_amount,
+          body.data.order.order_id,
+          body.data.payment.cf_payment_id,
+          body.data.order.order_amount,
         );
       }
-
       return res.status(200).send('OK');
     } catch (err) {
       console.error('Cashfree Webhook Error:', err);
